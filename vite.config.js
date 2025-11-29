a2d1c183ef5b1a7bb5117a09f31260b90cde9708
hoamxTrav <travis@hoamx.com>
2025-11-29 17:25:16 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,                         // allow external access (0.0.0.0)
-    port: 5173,
-    allowedHosts: ['ricelaketerrace.com', 'www.ricelaketerrace.com'],
-
-    // If HMR from external devices becomes unstable,
-    // you can enable these later:
-    // origin: 'http://ricelaketerrace.com',
-    // hmr: { host: 'ricelaketerrace.com', protocol: 'ws' },
-  },
 })
