8130b5674ed011a424872478db7efd5d2ca88774
hoamxTrav <travis@hoamx.com>
2025-11-29 16:35:00 -0600

    Delete components directory

--- components/MessageBubble.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function MessageBubble({ role, text }) {
-  const isUser = role === "user";
-  const base = "max-w-[85%] rounded-2xl px-3 py-2 shadow-sm text-sm";
-  const userStyles = "bg-blue-600 text-white rounded-br-md";
-  const botStyles = "bg-white border rounded-bl-md";
-  return <div className={`${base} ${isUser ? userStyles : botStyles}`}>{text}</div>;
-}
